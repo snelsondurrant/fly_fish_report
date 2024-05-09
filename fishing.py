2e1a73ef45e2c2497b2a38d76d4a94e2d4b424b5
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-09 11:36:05 -0400

    Added URLs

--- fishing.py
+++ fishing.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+VIVIAN_COUNT = 6
+PROVO_COUNT = 2
+HOBBLE_COUNT = 0
+
 url = "https://dwrapps.utah.gov/fishstocking/Fish?y=2024"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get(url, headers=headers)
@@ -9,13 +13,16 @@ soup = BeautifulSoup(response.text, "lxml")
 print()
 print("Running web crawler to check for UDWR fishing updates...")
 
-if str(soup).count("VIVIAN PARK P") > 6:
+if str(soup).count("VIVIAN PARK P") > VIVIAN_COUNT:
     print("Vivian Park Pond has been stocked! (new stock count: " + str(str(soup).count("VIVIAN PARK P")) + ")")
+    print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
 
-if str(soup).count("PROVO R") > 2:
+if str(soup).count("PROVO R") > PROVO_COUNT:
     print("The Provo River has been stocked! (new stock count: " + str(str(soup).count("PROVO R")) + ")")
+    print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
 
-if str(soup).count("HOBBLE CR") > 0:
+if str(soup).count("HOBBLE CR") > HOBBLE_COUNT:
     print("Hobble Creek has been stocked! (new stock count: " + str(str(soup).count("HOBBLE CR")) + ")")
+    print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
 
 print()
