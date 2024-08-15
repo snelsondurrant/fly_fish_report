e036cb3d6e89212f3a78ec294c8e8ee489cbe413
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-15 12:47:54 -0600

    Updates

--- fishing.py
+++ fishing.py
@@ -6,7 +6,7 @@ PROVO_COUNT = 4
 HOBBLE_COUNT = 0
 
 url = "https://dwrapps.utah.gov/fishstocking/Fish?y=2024"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}   
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 try:
   response = requests.get(url, headers=headers, timeout=5)
