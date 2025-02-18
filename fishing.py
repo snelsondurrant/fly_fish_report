87701637dc8124265838ccf7a950606f3e55f56d
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-18 16:21:03 -0700

    Made it waaay better

--- fishing.py
+++ fishing.py
@@ -1,37 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+import pickle
+import sys
+import os
 
-VIVIAN_COUNT = 14
-PROVO_COUNT = 4
-HOBBLE_COUNT = 0
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-url = "https://dwrapps.utah.gov/fishstocking/Fish?y=2024"
+with open("data.pickle", "rb") as file:
+    loaded_data = pickle.load(file)
+
+url = "https://dwrapps.utah.gov/fishstocking/Fish?y=2025"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-print()
-print("Running web crawler to check for UDWR fishing updates...")
-
 try:
-  response = requests.get(url, headers=headers, timeout=5)
+  response = requests.get(url, headers=headers, timeout=1)
   response.raise_for_status()
 except (requests.RequestException, requests.exceptions.Timeout) as e:
-  print("ERROR: Could not connect to the internet. Skipping update check.")
+  print()
+  print("[ERROR] Could not connect to the internet to check UDWR fly fishing data.")
+  print()
 else:
+
   soup = BeautifulSoup(response.text, "lxml")
+  vivian_new_cnt = str(soup).count("VIVIAN PARK P")
+  provo_new_cnt = str(soup).count("PROVO R")
 
-  if str(soup).count("VIVIAN PARK P") > VIVIAN_COUNT:
-      print()
-      print("Vivian Park Pond has been stocked! (new stock count: " + str(str(soup).count("VIVIAN PARK P")) + ")")
-      print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
+  if len(sys.argv) > 1 and sys.argv[1] == "--update":
 
-  if str(soup).count("PROVO R") > PROVO_COUNT:
-      print()
-      print("The Provo River has been stocked! (new stock count: " + str(str(soup).count("PROVO R")) + ")")
-      print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
+      loaded_data["vivian_cnt"] = vivian_new_cnt
+      loaded_data["provo_cnt"] = provo_new_cnt
+      with open("data.pickle", "wb") as file:
+          pickle.dump(loaded_data, file)
 
-  if str(soup).count("HOBBLE CR") > HOBBLE_COUNT:
-      print()
-      print("Hobble Creek has been stocked! (new stock count: " + str(str(soup).count("HOBBLE CR")) + ")")
-      print("https://dwrapps.utah.gov/fishstocking/Fish?y=2024")
+  else:
 
-print()
+    print()
+    found_flag = False
+
+    if loaded_data["vivian_cnt"] != vivian_new_cnt:
+        print("Vivian Park Pond has been stocked! (new stock count: " + str(vivian_new_cnt) + ")")
+        found_flag = True
+
+    if loaded_data["provo_cnt"] != provo_new_cnt:
+        print("The Provo River has been stocked! (new stock count: " + str(provo_new_cnt) + ")")
+        found_flag = True
+
+    if not found_flag:
+        print("No new UDWR fly fishing data found.")
+    else:
+        print(url)
+
+    print()
--- setup.bash
+++ setup.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 'python3 ~/fishing_web_crawler/fishing.py' >> ~/.bashrc
+sudo apt install python3-requests
+sudo apt install python3-bs4
+
+if grep -q 'python3 ~/fishing_web_crawler/fishing.py' ~/.bashrc; then
+    echo '[WARNING] Fly fishing script is already found in the .bashrc'
+else
+    echo 'python3 ~/fishing_web_crawler/fishing.py' >> ~/.bashrc
+fi
